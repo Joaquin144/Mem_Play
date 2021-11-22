a9535cb6391b42550649851f1031d5fd39587855
Joaquin144 <vvpp898.comco@gmail.com>
2021-11-22 19:11:16 +0530

    Now user can reset game.

--- app/src/main/java/com/vibhu/nitjsr/memplay/MainActivity.kt
+++ app/src/main/java/com/vibhu/nitjsr/memplay/MainActivity.kt
@@ -4,6 +4,8 @@ import android.animation.ArgbEvaluator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
@@ -31,26 +33,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        clRoot= findViewById(R.id.clRoot)
         rvBoard = findViewById(R.id.rvBoard)
         tvNumMoves = findViewById(R.id.tvNumMoves)
         tvNumPairs = findViewById(R.id.tvNumPairs)
-        tvNumPairs.setTextColor(ContextCompat.getColor(this,R.color.color_progress_none))
-        memoryGame = MemoryGame(boardSize)
-        clRoot= findViewById(R.id.clRoot)
-        //layout manager is responsible for measuring and positioning of items(here called as itemview) infalted in RV
-        rvBoard.layoutManager = GridLayoutManager(this,boardSize.getWidth());
-        //adapter ka kaam hai binfding provide karna RV items ke liye
-        adapter = MemoryBoardAdapter(this,boardSize,memoryGame.cards,object: MemoryBoardAdapter.CardClickListener{
-            override fun onCardClicked(position: Int) {
-                updateGameWithFlip(position)
-            }
 
-        })
-        rvBoard.adapter = adapter
-        rvBoard.setHasFixedSize(true) //--> This is performance optimisation (optional statement) -->Official Documentation :: I think jab itemviews mein hum koi change karte hain tab RV poora invalidate ho jata hai kyuki woh by deafult maan kar chalta hai ki uska size depends on its child elements. However generally low profile apps won't require this much sophistication. So they should prefer using setHasFixedSize function so that RV does not inflates all elements again and again when the content changes.
-
-
-
+        setUpBoard()
     }
 
     private fun updateGameWithFlip(position: Int) {
@@ -78,5 +66,35 @@ class MainActivity : AppCompatActivity() {
         }
         tvNumMoves.text = "Moves: ${memoryGame.getNumMoves()}"
         adapter.notifyDataSetChanged()//Neend mein soye hue adapter ko batao ki bhaiya badal do RV ko
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.mi_refresh -> {
+                //set up the game again
+                setUpBoard()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+    private fun setUpBoard(){
+        tvNumPairs.setTextColor(ContextCompat.getColor(this,R.color.color_progress_none))
+        memoryGame = MemoryGame(boardSize)
+        //layout manager is responsible for measuring and positioning of items(here called as itemview) infalted in RV
+        rvBoard.layoutManager = GridLayoutManager(this,boardSize.getWidth());
+        //adapter ka kaam hai binfding provide karna RV items ke liye
+        adapter = MemoryBoardAdapter(this,boardSize,memoryGame.cards,object: MemoryBoardAdapter.CardClickListener{
+            override fun onCardClicked(position: Int) {
+                updateGameWithFlip(position)
+            }
+
+        })
+        rvBoard.adapter = adapter
+        rvBoard.setHasFixedSize(true) //--> This is performance optimisation (optional statement) -->Official Documentation :: I think jab itemviews mein hum koi change karte hain tab RV poora invalidate ho jata hai kyuki woh by deafult maan kar chalta hai ki uska size depends on its child elements. However generally low profile apps won't require this much sophistication. So they should prefer using setHasFixedSize function so that RV does not inflates all elements again and again when the content changes.
     }
 }
